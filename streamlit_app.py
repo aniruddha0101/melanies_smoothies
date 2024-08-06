4ca32283581369f41af923f3ea724c03fc834f55
aniruddha0101 <aniruddhakadam3011@gmail.com>
2024-08-06 16:26:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,4 +74,4 @@ if st.button('Submit Order'):
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json)
